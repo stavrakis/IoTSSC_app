c53f1aed731d35ae704490d1604ab09fa1e6eefc
Stavros Christodoulou <ros2901@cytanet.com.cy>
2021-03-04 02:09:07 +0000

    view_db & allowed host

--- activity_tracker/urls.py
+++ activity_tracker/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('/', views.index, name='index'),
+    path('/view_db/', views.viewdb, name='view_db'),
 ]
--- activity_tracker/views.py
+++ activity_tracker/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SensorData
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the activity tracker index.")
+
+
+def viewdb(request):
+    limit = 0
+    limit_arg = request.GET.get('last')
+    if limit_arg and limit_arg < 5000:
+        limit = limit_arg + 1
+    else:
+        limit = 201
+
+    resp = SensorData.objects.all()[:limit]
+    out = '<br />'.join([r for r in resp])
+    return HttpResponse(out)

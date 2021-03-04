e2ff083e3852a402e829d900155fa9dbf9f00a64
Stavros Christodoulou <ros2901@cytanet.com.cy>
2021-03-04 18:17:55 +0000

    pub sub

--- activity_tracker/views.py
+++ activity_tracker/views.py
@@ -29,8 +29,7 @@ def pub(request):
     print('pub called')
     if request.method == 'POST':
 
-
-        data = request.POST
+        data = request.data.decode('utf-8')
         print(data)
         #pubsub_message = base64.b64decode(data.decode('utf-8'))
         #jsondata = json.loads(pubsub_message)
